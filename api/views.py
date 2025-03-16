e7d12ba03d60864540db2a018537571a2f0548be
Pamela Estrada <pameg@192.168.1.84>
2025-03-16 10:26:10 -0600

    final changes

--- api/views.py
+++ api/views.py
@@ -1,37 +1,134 @@
 import requests
 
-
 class PokemonView():
 
     def create(self, name):
-        response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+        """
+        Method that gets pokemon data from an external API
+        :param name: pokemon name
+        :return: pokemon data as dict
+        """
+        try:
+            response = requests.get(f'https://pokeapi.co/api/v2/pokemon/{name}')
+        except Exception as e:
+            raise f"Error while connecting to Pokemon external API {str(e)}"
+
         data = response.json()
-        self._create_pokemon_data(data)
+        data = self._create_pokemon_data(data)
+        return data
 
     def _create_pokemon_data(self, data):
+        """
+        Method that formats received data
+        :param data: api data
+        :return: pokemon data as dict
+        """
 
-        pokemon_types = self._parse_types_data(data['types'])
         pokemon_moves = self._parse_moves_data(data['moves'])
+        pokemon_types = self._parse_types_data(data['types'])
+        pokemon_sprites = self._parse_sprites_data(data['sprites'])
+        pokemon_abilities = self._parse_abilities_data(data['abilities'])
+        pokemon_stats = self._parse_stats_data(data['stats'])
 
         pokemon_data = {'id': data['id'],
                         'name': data['name'],
                         'weight': data['weight'],
                         'height': data['height'],
                         'types': pokemon_types,
-                        'moves': pokemon_moves}
-        print(pokemon_data)
+                        'moves': pokemon_moves,
+                        'sprites': pokemon_sprites,
+                        'abilities': pokemon_abilities,
+                        'stats': pokemon_stats}
+        return pokemon_data
 
     def _parse_types_data(self, types_data):
+        """
+        Method that formats types data
+        :param types_data:
+        :return: formated dict
+        """
         types = []
         for ptype in types_data:
             types.append(ptype['type']['name'])
         return types
 
     def _parse_moves_data(self, moves_data):
+        """
+        Method that formats moves data
+        :param moves_data: dict
+        :return: formated dict
+        """
         moves = []
         for move in moves_data:
             moves.append(move['move']['name'])
         return moves
+
+    def _parse_sprites_data(self, sprites_data):
+        """
+        Method that formats sprites data
+        :param sprites_data: dict
+        :return: formated dict
+        """
+        sprites  = []
+        if sprites_data['back_default']:
+            sprites.append(sprites_data['back_default'])
+            sprites.append(sprites_data['back_shiny'])
+            sprites.append(sprites_data['front_default'])
+            sprites.append(sprites_data['front_shiny'])
+        elif sprites_data['back_female']:
+            sprites.append(sprites_data['back_female'])
+            sprites.append(sprites_data['back_shiny_female'])
+            sprites.append(sprites_data['front_female'])
+            sprites.append(sprites_data['front_shiny_female'])
+
+        return sprites
+
+    def _parse_abilities_data(self, abilities_data):
+        """
+        Metdod that formats abilities data
+        :param abilities_data: dict
+        :return: formated dict
+        """
+        abilities = {}
+        for ability in abilities_data:
+            descriptions = self._get_abilities_description(ability['ability']['url'])
+            abilities.update({ability['ability']['name']:descriptions})
+        return abilities
+
+    def _get_abilities_description(self, ab_url):
+        """
+        Method that formats abilities description, need to fetch the
+        data from an external API
+        :param ab_url: url for fetching data
+        :return: formated dict
+        """
+        resp = requests.get(ab_url)
+        effects_data = resp.json()
+        effects = []
+        effect_change_data = effects_data['effect_changes']
+        for effect_change in effect_change_data:
+            for eff in effect_change['effect_entries']:
+                if eff['language']['name'] == 'en':
+                    effects.append(eff['effect'])
+
+        for effect_entry in effects_data['effect_entries']:
+            if effect_entry['language']['name'] == 'en':
+                effects.append(effect_entry['short_effect'])
+        return effects
+
+    def _parse_stats_data(self, stats_data):
+        """
+        Method that formats stats data
+        :param stats_data: dict
+        :return: formated dict
+        """
+        stats = []
+        for stat in stats_data:
+            stats.append(stat['stat']['name'])
+        return stats
+
+
+
 
 
 
--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask
+from flask import Flask, Response
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import json
 
-from api.views import PokemonView
+import api.views as view
 
 app = Flask(__name__)
 
@@ -10,6 +11,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///pokemonAPI.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
+
+class Pokemon(db.Model):
+    """
+    Model for Pokemon data
+    """
+    api_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    weight = db.Column(db.Float)
+    height = db.Column(db.Float)
+    moves = db.Column(db.JSON)
+    types = db.Column(db.JSON)
+    sprites = db.Column(db.JSON)
+    abilities = db.Column(db.JSON)
+    stats = db.Column(db.JSON)
 
 with app.app_context():
     db.create_all()
@@ -20,10 +35,80 @@ migrate.init_app(app,db)
 
 @app.route("/create_pokemon/<poke_name>")
 def create_pokemon(poke_name):
+    """
+    Endpoint that stores the pokemon data into de db
+    :param poke_name: <pokemon name>
+    :return: pokemon_name, status
+    """
     name = poke_name
-    pokemon =  PokemonView()
+    pokemon =  view.PokemonView()
     poke_data = pokemon.create(name)
-    print(poke_data)
+    new_pokemon = Pokemon(api_id=1,#poke_data['id'],
+                          name=poke_data['name'],
+                          weight=poke_data['weight'],
+                          height=poke_data['height'],
+                          moves=poke_data['moves'],
+                          types=poke_data['types'],
+                          sprites=poke_data['sprites'],
+                          abilities=poke_data['abilities'],
+                          stats=poke_data['stats'])
+    try:
+        db.session.add(new_pokemon)
+        db.session.commit()
+        return Response(f'Pokemon {new_pokemon.name} created',status=200)
+    except Exception as e:
+        return Response(f'Error while creating a new pokemon {str(e)}',
+                        status=400)
+
+@app.route("/display_pokemon/<poke_name>")
+def display_pokemon(poke_name):
+    """
+    Endpoint that shows a single pokemon data
+    :param poke_name: <pokemon name>
+    :return: pokemon object
+    """
+    name = poke_name
+    try:
+        pokemon_data = Pokemon.query.filter_by(name=name.lower()).first()
+        pokemon = {'id':pokemon_data.api_id,
+                   'name': pokemon_data.name,
+                   'weight': pokemon_data.weight,
+                   'height': pokemon_data.height,
+                   'moves': pokemon_data.moves,
+                   'types': pokemon_data.types,
+                   'sprites': pokemon_data.types,
+                   'abilities': pokemon_data.abilities,
+                   'stats': pokemon_data.stats
+                 }
+        return Response(json.dumps(pokemon),status=200)
+    except Exception as e:
+        return Response(f'Error while fetching pokemon{str(e)}',
+                        status=400)
+
+@app.route("/display_all_pokemons/")
+def display_all():
+    """
+    Endpoint that shows all stored pokemons
+    :return: Pokemons data object
+    """
+    pokemons = []
+    try:
+        pokemon_data = Pokemon.query.all()
+        for pokemon in pokemon_data:
+            pokemons.append({'id':pokemon.api_id,
+                             'name': pokemon.name,
+                             'weight': pokemon.weight,
+                             'height': pokemon.height,
+                             'moves': pokemon.moves,
+                             'types': pokemon.types,
+                             'sprites': pokemon.types,
+                             'abilities': pokemon.abilities,
+                             'stats': pokemon.stats
+                             })
+        return Response(json.dumps(pokemons), status=200)
+    except Exception as e:
+        return Response(f'Error while fetching pokemons{str(e)}',
+                        status=400)
 
 if __name__ == '__main__':
     app.run(debug=True)
